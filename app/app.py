b33e8fba84c58db10699913a66c4946e3b5a9bd1
Mihaila Valerio <mihaila.andrei95@gmail.com>
2018-05-02 17:40:12 +0300

    azure postgre

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 APP.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://%s:%s@%s/%s' % (
     # ARGS.dbuser, ARGS.dbpass, ARGS.dbhost, ARGS.dbname
-    os.environ['DBUSER'], os.environ['DBPASS'], os.environ['DBHOST'], os.environ['DBNAME']
+    "mihaila@ccdbhw4","adminadmin123?","ccdbhw4.postgres.database.azure.com","eventregistration"
 )
 
 # initialize the database connection
